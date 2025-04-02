d35942c18ab40c79378c0989c7c6dc1c572fda3c
Francisco Sandoval <fsandovalhernandez.l0@gmail.com>
2025-04-02 18:07:12 +0200

    Refactor project structure and implement user authentication routes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,4 @@
-const app = require('../../backend/src/app'); // adjust path to point to your app.js
+const app = require('./src/app'); // adjust path to point to your app.js
 const serverless = require('serverless-http');
 
 module.exports = serverless(app);
